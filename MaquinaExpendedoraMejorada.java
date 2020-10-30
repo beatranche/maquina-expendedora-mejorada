76a653fb88446f4767e03c19855325a36841dbbd
beatranche <56152901+beatranche@users.noreply.github.com>
2020-10-30 09:59:12 +0100

    0745.01 corrección

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -54,13 +54,8 @@ public class MaquinaExpendedoraMejorada {
      * Imprime un billete para el cliente actual
      */
     public void imprimirBillete() {
-        int cantidadDeDineroQueFalta = 0;
-        if (balanceClienteActual < precioBillete){
-            cantidadDeDineroQueFalta = precioBillete - balanceClienteActual;
-            System.out.println("Faltan " + cantidadDeDineroQueFalta + " euros");
-            }
-            
-        if (balanceClienteActual >= precioBillete) {        
+        int cantidadDeDineroQueFalta = precioBillete - balanceClienteActual;
+        if (cantidadDeDineroQueFalta <=0) {        
             // Simula la impresion de un billete
             System.out.println("##################");
             System.out.println("# Billete de tren:");
